eb34430ab5ea51dfd81f5b2196a17b9cc0e36655
John Shin <john.shin53@gmail.com>
2020-10-25 18:43:07 -0400

    updated db IRO to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://301099013:PPnxSPDlueFLGzJa@mongodbserver.oc68l.mongodb.net/books229?retryWrites=true&w=majority"
 };
