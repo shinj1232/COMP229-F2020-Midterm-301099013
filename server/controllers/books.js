36209602b5ca93b7f2ca18727513bad69b722152
John Shin <john.shin53@gmail.com>
2020-10-25 21:34:28 -0400

    created controller for the routes

---
+++ server/controllers/books.js
@@ -0,0 +1,123 @@
+//Hyunjune Shin 301099013
+let express = require("express");
+let router = express.Router();
+let mongoose = require('mongoose');
+
+//define the book model
+let book = require('../models/books');
+
+module.exports.displayBookList =(req, res, next) => {
+    // find all books in the books collection
+    book.find( (err, books) => {
+      if (err) {
+        return console.error(err);
+      }
+      else {
+        res.render('books/index', {
+          title: 'Books',
+          books: books
+        });
+      }
+    });
+}
+
+//displays empty details page to add
+module.exports.displayAddPage = (req, res, next) => {
+
+  res.render('books/details', {title: 'Add', books: ''});
+
+}
+
+//processesing the new book entry from details page
+module.exports.processAddPage = (req, res, next) => {
+
+  //initialize new book 
+  let newBook = book({
+    "Title": req.body.title,
+    "Price": req.body.price,
+    "Author": req.body.author,
+    "Genre": req.body.genre
+  });
+
+  //create new book
+  book.create(newBook, (err,book)=>{
+    if(err)
+    {
+      console.log(err);
+      res.end(err);
+    } else {
+      //goes back to the book list page
+      res.redirect('/books');
+    }
+  });
+}
+
+//display details page with the info that user wants to edit
+module.exports.displayEditPage  = (req, res, next) => {
+
+  //initialize id
+  let id = req.params.id;
+  //find the book by id
+  book.findById(id, (err, bookToEdit ) =>{
+    if(err)
+    {
+        console.log(err);
+        res.end(err);
+    }
+    else 
+    {
+      //sends the retrived data of the id and goes to details with correct info
+        res.render('books/details', {title: 'Edit Book',  books: bookToEdit});
+    }
+});
+
+}
+
+//processing the edit
+module.exports.processEditPage = (req, res, next) => {
+
+  //initialize the id
+  let id = req.params.id;
+  //initialize book
+  let updatedBook = book({
+    "_id": id,
+    "Title": req.body.title,
+    "Price": req.body.price,
+    "Author": req.body.author,
+    "Genre": req.body.genre
+  });
+  //updates the edited book
+  book.updateOne({_id: id,}, updatedBook,(err)=>{
+    if(err)
+    {
+        console.log(err);
+        res.end(err);
+    }
+    else 
+    {
+      //sends to user back to the list of books with updated list
+        res.redirect('/books');
+    }
+  });
+}
+
+//performs delete of the data
+module.exports.performDelete =  (req, res, next) => {
+
+  //initialize id
+  let id = req.params.id;
+  //removes the book by its' id
+  book.remove({_id: id}, (err)=> {
+    if(err)
+    {
+        console.log(err);
+        res.end(err);
+    }
+    else
+    {
+      //sends the user back to the list of books with updated list
+        res.redirect('/books');
+    }
+  });
+}
+
--- server/routes/books.js
+++ server/routes/books.js
@@ -6,127 +6,24 @@ let mongoose = require('mongoose');
 // define the book model
 let book = require('../models/books');
 
+let bookController = require("../controllers/books");
 /* GET books List page. READ */
-router.get('/', (req, res, next) => {
-  // find all books in the books collection
-  book.find( (err, books) => {
-    if (err) {
-      return console.error(err);
-    }
-    else {
-      res.render('books/index', {
-        title: 'Books',
-        books: books
-      });
-    }
-  });
-
-});
+router.get('/', bookController.displayBookList);
 
 //  GET the Book Details page in order to add a new Book
-router.get('/add', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-    res.render('books/details', {title: 'Add', books: ''});
-
-});
+router.get('/add', bookController.displayAddPage);
 
 // POST process the Book Details page and create a new Book - CREATE
-router.post('/add', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-    let newBook = book({
-      "Title": req.body.title,
-      "Price": req.body.price,
-      "Author": req.body.author,
-      "Genre": req.body.genre
-    });
-
-    book.create(newBook, (err,book)=>{
-      if(err)
-      {
-        console.log(err);
-        res.end(err);
-      } else {
-        res.redirect('/books');
-      }
-    });
-});
+router.post('/add', bookController.processAddPage);
 
 // GET the Book Details page in order to edit an existing Book
-router.get('/:id', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-    let id = req.params.id;
-    book.findById(id, (err, bookToEdit ) =>{
-      if(err)
-      {
-          console.log(err);
-          res.end(err);
-      }
-      else 
-      {
-          res.render('books/details', {title: 'Edit Book',  books: bookToEdit});
-      }
-  });
-
-});
+router.get('/:id', bookController.displayEditPage);
 
 // POST - process the information passed from the details form and update the document
-router.post('/:id', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-    let id = req.params.id;
-    let updatedBook = book({
-      "_id": id,
-      "Title": req.body.title,
-      "Price": req.body.price,
-      "Author": req.body.author,
-      "Genre": req.body.genre
-    });
-    book.updateOne({_id: id,}, updatedBook,(err)=>{
-      if(err)
-      {
-          console.log(err);
-          res.end(err);
-      }
-      else 
-      {
-          res.redirect('/books');
-      }
-  });
-
-
-
-});
+router.post('/:id', bookController.processEditPage);
 
 // GET - process the delete by user id
-router.get('/delete/:id', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-    let id = req.params.id;
-    book.remove({_id: id}, (err)=> {
-      if(err)
-      {
-          console.log(err);
-          res.end(err);
-      }
-      else
-      {
-          res.redirect('/books');
-      }
-    });
-});
+router.get('/delete/:id', bookController.performDelete);
 
 
 module.exports = router;
